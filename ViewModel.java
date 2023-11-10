917940db316d2edadc342e5d934aabb83837161f
Dragorus <150255031+Dragorus@users.noreply.github.com>
2023-11-10 15:55:25 +0700

    view_model_commit1

---
+++ ViewModel.java
@@ -0,0 +1,10 @@
+package com.example.mob_app_lab5;
+
+public class ViewModel extends androidx.lifecycle.ViewModel {
+
+    
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+    }
+}
